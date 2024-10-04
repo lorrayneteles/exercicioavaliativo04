8650d1ad25bf69ee842178385e05f0df8b2e43c7
lorrayneteles <lorrayneteles1305@gmail.com>
2024-10-04 20:56:19 -0300

    Add files via upload

---
+++ exercicioavaliaivo04.py
@@ -0,0 +1,7 @@
+# Peça ao usuario dois numeros e mostre a divisão deles
+n1 = int(input("Digite o primeiro número:\n"))
+n2 = int(input("Digite o segundo número:\n"))
+
+divisao = ( n1/n2)
+
+print(" A divisão de dois números é:\n", divisao)
